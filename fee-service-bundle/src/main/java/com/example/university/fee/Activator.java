5b097bb837885cab1d9ec1451563a4a2295846bc
amirsamss <amustaqim345@gmail.com>
2026-01-25 12:42:53 +0800

    finalized fee-service

---
+++ fee-service-bundle/src/main/java/com/example/university/fee/Activator.java
@@ -0,0 +1,26 @@
+package com.example.university.fee;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Fee Service Bundle Activator
+ */
+public class Activator implements BundleActivator {
+
+    private static final Logger logger = LoggerFactory.getLogger(Activator.class);
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        logger.info("Fee Service Bundle starting...");
+        logger.info("Fee Service Bundle started successfully");
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        logger.info("Fee Service Bundle stopping...");
+        logger.info("Fee Service Bundle stopped");
+    }
+}
